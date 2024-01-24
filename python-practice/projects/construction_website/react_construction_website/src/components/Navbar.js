def65ca2d2fbffdea8b9f11011961850b8228934
Norbert Blazejewski (ZJAC006) <ZJAC006@live.rhul.ac.uk>
2024-01-24 11:02:37 +0000

    Edited navigation bar.
    Added style to the navigation bar. Additionally, made it responsive for smaller screen size.

--- python-practice/projects/construction_website/react_construction_website/src/components/Navbar.js
+++ python-practice/projects/construction_website/react_construction_website/src/components/Navbar.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
+
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
+
     return (
-        <div className='div-container'>
-            <nav>
-                <Link to="/"> <h1 className="store-name">LOGO</h1> </Link>
-                <ul className="tabs">
-                    <li>Services</li>
-                    <li>Portfolio</li>
-                    <li>Contact</li>
-                </ul>
+        <div>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to="/"> <h1 className="store-name">LOGO</h1> </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className="nav-item">
+                            <Link to="/" className='nav-links' onClick={closeMobileMenu}>Services</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/" className='nav-links' onClick={closeMobileMenu}>Portfolio</Link>
+                        </li>
+                        <li className="nav-item"> 
+                            <Link to="/" className='nav-links' onClick={closeMobileMenu}>Contact</Link>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </div>
     )
